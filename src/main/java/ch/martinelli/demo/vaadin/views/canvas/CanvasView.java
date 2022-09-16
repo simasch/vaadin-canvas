bb39a32843c6c98d0c0c79a148561b91c6054dd4
Simon Martinelli <simon@72.services>
2022-09-16 09:22:05 +0200

    Initial commit

--- src/main/java/ch/martinelli/demo/vaadin/views/canvas/CanvasView.java
+++ src/main/java/ch/martinelli/demo/vaadin/views/canvas/CanvasView.java
@@ -1,9 +1,8 @@
 package ch.martinelli.demo.vaadin.views.canvas;
 
 import ch.martinelli.demo.vaadin.views.MainLayout;
-import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.Html;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -15,19 +14,156 @@ import com.vaadin.flow.router.RouteAlias;
 public class CanvasView extends VerticalLayout {
 
     public CanvasView() {
-        setSpacing(false);
+        Html canvas = new Html("""
+                <main>
+                  <div class="left-block">
+                    <div class="colors">
+                      <button type="button" value="#0000ff"></button>
+                      <button type="button" value="#009fff"></button>
+                      <button type="button" value="#0fffff"></button>
+                      <button type="button" value="#bfffff"></button>
+                      <button type="button" value="#000000"></button>
+                      <button type="button" value="#333333"></button>
+                      <button type="button" value="#666666"></button>
+                      <button type="button" value="#999999"></button>
+                      <button type="button" value="#ffcc66"></button>
+                      <button type="button" value="#ffcc00"></button>
+                      <button type="button" value="#ffff00"></button>
+                      <button type="button" value="#ffff99"></button>
+                      <button type="button" value="#003300"></button>
+                      <button type="button" value="#555000"></button>
+                      <button type="button" value="#00ff00"></button>
+                      <button type="button" value="#99ff99"></button>
+                      <button type="button" value="#f00000"></button>
+                      <button type="button" value="#ff6600"></button>
+                      <button type="button" value="#ff9933"></button>
+                      <button type="button" value="#f5deb3"></button>
+                      <button type="button" value="#330000"></button>
+                      <button type="button" value="#663300"></button>
+                      <button type="button" value="#cc6600"></button>
+                      <button type="button" value="#deb887"></button>
+                      <button type="button" value="#aa0fff"></button>
+                      <button type="button" value="#cc66cc"></button>
+                      <button type="button" value="#ff66ff"></button>
+                      <button type="button" value="#ff99ff"></button>
+                      <button type="button" value="#e8c4e8"></button>
+                      <button type="button" value="#ffffff"></button>
+                    </div>
+                    <div class="brushes">
+                      <button type="button" value="1"></button>
+                      <button type="button" value="2"></button>
+                      <button type="button" value="3"></button>
+                      <button type="button" value="4"></button>
+                      <button type="button" value="5"></button>
+                    </div>
+                    <div class="buttons">
+                      <button id="clear" type="button">Clear</button>
+                      <button id="save" type="button">Save</button>
+                    </div>
+                  </div>
+                  <div class="right-block">
+                    <canvas id="paint-canvas" width="640" height="400"></canvas>
+                  </div>
+                </main>                                                      
+                """);
+        add(canvas);
 
-        Image img = new Image("images/empty-plant.png", "placeholder plant");
-        img.setWidth("200px");
-        add(img);
+        Button save = new Button("Save");
+        save.setId("save");
+        add(save);
 
-        add(new H2("This place intentionally left empty"));
-        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+        Button clear = new Button("Clear");
+        clear.setId("clear");
+        add(clear);
 
-        setSizeFull();
-        setJustifyContentMode(JustifyContentMode.CENTER);
-        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
-        getStyle().set("text-align", "center");
+        initPaint();
+    }
+
+    public void initPaint() {
+        getElement().executeJs("""
+                this.initPaint = function () {
+                  // Definitions
+                  var canvas = document.getElementById("paint-canvas");
+                  var context = canvas.getContext("2d");
+                  var boundings = canvas.getBoundingClientRect();
+                                
+                  // Specifications
+                  var mouseX = 0;
+                  var mouseY = 0;
+                  context.strokeStyle = 'black'; // initial brush color
+                  context.lineWidth = 1; // initial brush width
+                  var isDrawing = false;
+                                
+                                
+                  // Handle Colors
+                  var colors = document.getElementsByClassName('colors')[0];
+                                
+                  colors.addEventListener('click', function(event) {
+                    context.strokeStyle = event.target.value || 'black';
+                  });
+                                
+                  // Handle Brushes
+                  var brushes = document.getElementsByClassName('brushes')[0];
+                                
+                  brushes.addEventListener('click', function(event) {
+                    context.lineWidth = event.target.value || 1;
+                  });
+                                
+                  // Mouse Down Event
+                  canvas.addEventListener('mousedown', function(event) {
+                    setMouseCoordinates(event);
+                    isDrawing = true;
+                                
+                    // Start Drawing
+                    context.beginPath();
+                    context.moveTo(mouseX, mouseY);
+                  });
+                                
+                  // Mouse Move Event
+                  canvas.addEventListener('mousemove', function(event) {
+                    setMouseCoordinates(event);
+                                
+                    if(isDrawing){
+                      context.lineTo(mouseX, mouseY);
+                      context.stroke();
+                    }
+                  });
+                                
+                  // Mouse Up Event
+                  canvas.addEventListener('mouseup', function(event) {
+                    setMouseCoordinates(event);
+                    isDrawing = false;
+                  });
+                                
+                  // Handle Mouse Coordinates
+                  function setMouseCoordinates(event) {
+                    // Subtract navigation and tools
+                    mouseX = event.clientX - boundings.left - 432;
+                    // Subtract header
+                    mouseY = event.clientY - boundings.top - 71;
+                  }
+                                
+                  // Handle Clear Button
+                  var clearButton = document.getElementById('clear');
+                                
+                  clearButton.addEventListener('click', function() {
+                    context.clearRect(0, 0, canvas.width, canvas.height);
+                  });
+                                
+                  // Handle Save Button
+                  var saveButton = document.getElementById('save');
+                                
+                  saveButton.addEventListener('click', function() {
+                    var imageName = prompt('Please enter image name');
+                    var canvasDataURL = canvas.toDataURL();
+                    var a = document.createElement('a');
+                    a.href = canvasDataURL;
+                    a.download = imageName || 'drawing';
+                    a.click();
+                  });
+                };
+                this.initPaint();
+                """);
     }
 
 }
