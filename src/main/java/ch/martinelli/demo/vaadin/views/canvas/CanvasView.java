aacfe17ca084bfe9f8313f7bbbb54f69e8ffeef2
start.vaadin.com <no-reply@vaadin.com>
2022-09-16 06:32:47 +0000

    Generated project

---
+++ src/main/java/ch/martinelli/demo/vaadin/views/canvas/CanvasView.java
@@ -0,0 +1,33 @@
+package ch.martinelli.demo.vaadin.views.canvas;
+
+import ch.martinelli.demo.vaadin.views.MainLayout;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+
+@PageTitle("Canvas")
+@Route(value = "canvas", layout = MainLayout.class)
+@RouteAlias(value = "", layout = MainLayout.class)
+public class CanvasView extends VerticalLayout {
+
+    public CanvasView() {
+        setSpacing(false);
+
+        Image img = new Image("images/empty-plant.png", "placeholder plant");
+        img.setWidth("200px");
+        add(img);
+
+        add(new H2("This place intentionally left empty"));
+        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+
+        setSizeFull();
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        getStyle().set("text-align", "center");
+    }
+
+}
